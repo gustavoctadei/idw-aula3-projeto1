32d279686d2ccdcc0aa254d22bed5839a4fcb8f1
Gustavo Tadei <gustavo.tadei@hotmail.com>
2021-07-06 21:10:25 -0400

    commit inicial

---
+++ src/main/java/br/unir/jsf/bean/FornecedorBean.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unir.jsf.bean;
+
+import java.io.Serializable;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.inject.Named;
+
+/**
+ *
+ * @author Gustavo
+ */
+
+@ManagedBean
+@Named
+@SessionScoped
+public class FornecedorBean implements Serializable {
+    
+    private String nome;
+    private String cnpj;
+    private String cidade;
+    
+    public String novo() {
+        return "fornecedor";
+    }
+    
+    public String salvar() {
+        return "mostraFornecedor";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCnpj() {
+        return cnpj;
+    }
+
+    public void setCnpj(String cnpj) {
+        this.cnpj = cnpj;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+    
+    
+    
+}
---
+++ src/main/java/br/unir/jsf/bean/UsuarioBean.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.unir.jsf.bean;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+
+
+/**
+ *
+ * @author Gustavo
+ */
+
+@ManagedBean
+@Named
+@SessionScoped
+public class UsuarioBean implements Serializable {
+    
+    private String nome;
+    private String email;
+    private String senha;
+    private String confirmaSenha;
+    private Date dataNascimento;
+    
+    public String novo() {
+        return "usuario";
+    }
+    
+    public String salvar() {
+        FacesContext context = FacesContext.getCurrentInstance();
+        
+        if (!this.senha.equals(this.confirmaSenha)) {
+            context.addMessage(null, new FacesMessage(FacesMessage.SEVERITY_WARN, "Senha não confirmada", ""));
+            return "usuario";
+        }
+        
+        return "mostraUsuario";
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getConfirmaSenha() {
+        return confirmaSenha;
+    }
+
+    public void setConfirmaSenha(String confirmaSenha) {
+        this.confirmaSenha = confirmaSenha;
+    }
+
+    public Date getDataNascimento() {
+        return dataNascimento;
+    }
+
+    public void setDataNascimento(Date dataNascimento) {
+        this.dataNascimento = dataNascimento;
+    }
+    
+}
